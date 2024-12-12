206066ccc814a4890eeea890035e21854fd2555c
Huy Phung <huyphung3103@gmail.com>
2024-12-12 00:23:34 -0500

    add explore page

---
+++ src/fakeData/explore.js
@@ -0,0 +1,84 @@
+const exploreData = {
+    "Gaming": [
+        {
+            "name": "Pro Gamers Unite",
+            "members": 2639,
+            "about": "For gaming enthusiasts who love to compete and share their gaming experiences.",
+            "image": "https://fastly.picsum.photos/id/0/5000/3333.jpg?hmac=_j6ghY5fCfSD6tvtcV74zXivkJSPIfR9B8w34XeQmvU"
+        },
+        {
+            "name": "Gamers Assemble",
+            "members": 5238,
+            "about": "Discuss games, share tips, and connect with fellow gamers.",
+            "image": "https://fastly.picsum.photos/id/96/4752/3168.jpg?hmac=KNXudB1q84CHl2opIFEY4ph12da5JD5GzKzH5SeuRVM"
+        },
+        {
+            "name": "Gamers Assemble",
+            "members": 5238,
+            "about": "Discuss games, share tips, and connect with fellow gamers.",
+            "image": "https://fastly.picsum.photos/id/14/2500/1667.jpg?hmac=ssQyTcZRRumHXVbQAVlXTx-MGBxm6NHWD3SryQ48G-o"
+        },
+    ],
+    "Music": [
+        {
+            "name": "Fans of Classic Rock Music",
+            "members": 8355,
+            "about": "Share and discuss your favorite classic rock music and artists.",
+            "image": "https://fastly.picsum.photos/id/836/5000/3333.jpg?hmac=vERoknhLmVMMlcmXhqfS1YvkbPIqt6cyMnU-cLJKddk"
+        },
+        {
+            "name": "Jazz Enthusiasts",
+            "members": 2890,
+            "about": "A place for jazz lovers to share and discuss their favorite jazz music and artists.",
+            "image": "https://fastly.picsum.photos/id/334/2304/1536.jpg?hmac=ihrh84KbSSW-zvp-oRz8FGCVOWvPAD3SZvYPS0v3mv8"
+        },
+        {
+            "name": "Music Lovers Unite",
+            "members": 4512,
+            "about": "Discover new tunes, share your favorite tracks, and connect with other music lovers.",
+            "image": "https://fastly.picsum.photos/id/861/5000/3333.jpg?hmac=gtGlGlhgov_p1Qv5alPB5qMx2Oen2YHFiMft8S_Znfs"
+        }
+    ],
+    "Fitness": [
+        {
+            "name": "Fitness Freaks Community",
+            "members": 3200,
+            "about": "Join us to share fitness tips, workout routines, and stay motivated.",
+            "image": "https://fastly.picsum.photos/id/101/2621/1747.jpg?hmac=cu15YGotS0gIYdBbR1he5NtBLZAAY6aIY5AbORRAngs"
+        },
+        {
+            "name": "Virtual Fitness Buddies",
+            "members": 204,
+            "about": "Stay fit together by sharing workout routines and fitness goals.",
+            "image": "https://fastly.picsum.photos/id/274/3824/2520.jpg?hmac=OOl_w8LX_psogyruUe1z986AuqeS_TY7rLxAFgG4wrc"
+        },
+        {
+            "name": "Fitness Enthusiasts Hub",
+            "members": 1856,
+            "about": "Share your fitness routines, goals, and achievements with fellow enthusiasts.",
+            "image": "https://fastly.picsum.photos/id/389/4562/3042.jpg?hmac=vnZGl2BO_TMZ7PmMnfvGQ69vNYy4auG7odEZWpc6cOA"
+        }
+    ],
+    "Education": [
+        {
+            "name": "Creative Minds Learning",
+            "members": 9522,
+            "about": "Learn and innovate with like-minded individuals who are passionate about education.",
+            "image": "https://fastly.picsum.photos/id/163/2000/1333.jpg?hmac=htdHeSJwlYOxS8b0TTpz2s8tD_QDlmsd3JHYa_HGrg8"
+        },
+        {
+            "name": "Lifelong Learners Group",
+            "members": 6789,
+            "about": "A community for those who believe in continuous learning and self-improvement.",
+            "image": "https://fastly.picsum.photos/id/192/2352/2352.jpg?hmac=jN5UExysObV7_BrOYLdxoDKzm_c_lRM6QkaInKT_1Go"
+        },
+        {
+            "name": "Knowledge Seekers Club",
+            "members": 4213,
+            "about": "Explore and share ideas, knowledge, and educational resources.",
+            "image": "https://fastly.picsum.photos/id/832/5000/3333.jpg?hmac=hFHyNp_cIcOMyxpCcTWRd_UM1QEHUyxu4PYl1l7c_fk"
+        }
+    ],
+}
+
+export default exploreData;
--- src/views/Explore.vue
+++ src/views/Explore.vue
@@ -1,11 +1,91 @@
 <template>
-    <div>
-        <h1>Explore Page</h1>
+    <div class="explore">
+        <div v-for="(category, index) in exploreData" :key="index" class="category-content">
+            <h2 class="category-name">{{ index }}</h2>
+            <div class="category-grid">
+                <div v-for="(group, groupIndex) in category" :key="groupIndex" class="group-card">
+                    <img :src="group.image" alt="Group Image" class="group-image" />
+                    <h3 class="group-name">{{ group.name }}</h3>
+                    <p class="group-about">{{ group.about }}</p>
+                    <p class="group-member"><strong>Members:</strong> {{ group.members }}</p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import exploreData from '../fakeData/explore.js';
+
 export default {
     name: 'Explore',
+    data() {
+        return {
+            exploreData: exploreData
+        }
+    }
 };
 </script>
+
+<style scoped>
+.explore {
+    position: relative;
+    width: 100%;
+    background-color: aliceblue;
+}
+
+.category-content {
+    text-align: center;
+}
+
+.category-name {
+    background-color: white;
+    font-size: 40px;
+    margin: 30px 0 ;
+    padding: 20px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+}
+
+.category-grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, 400px);
+    gap: 40px;
+    padding: 50px;
+}
+
+.group-card {
+    border-radius: 10px;
+    background-color: white;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+    transition: transform 0.2s ease-in-out;
+    cursor: pointer;
+}
+
+.group-card:hover {
+    transform: scale(1.02);
+}
+
+.group-card img {
+    width: 90%;
+    border-radius: 10px;
+}
+
+.group-name {
+    margin: 10px 0;
+    color: red;
+}
+
+.group-about {
+    margin: 10px 50px;
+    text-align: center;
+}
+
+.group-member {
+    margin-top: 10px 0;
+}
+</style>
